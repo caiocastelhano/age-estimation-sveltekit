f7bdabefb47adafc47773d2c93a9e3fd0750806d
Caio Castelhano <castelhanoc@gmail.com>
2025-06-29 20:41:01 -0300

    Implementa debounce com atraso de 1000ms após digitação

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,27 @@
-<!-- let: cria a variável name com valor vazio. -->
+<!-- let name: cria a variável name com valor vazio, diretamente ligada ao input pelo bind:value. -->
+<!-- let debouncedName: cria a variável debouncedName com valor vazio - só é atualizada depois da espera. -->
+<!-- let debounceTimeout: cria a referência do tempo de espera -->
+
+<!-- tempo de espera anterior é cancelado -->
+<!-- novo timeout é criado com 1000ms -->
+<!-- se o tempo passar e o usuário não digitar de novo, debouncedName é atualizado com o valor de name -->
+
 <script>
-  let name = ""; 
+  let name = "";
+  let debouncedName = "";
+  let debounceTimeout;
+
+  $: {
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(() => {
+      debouncedName = name; 
+    }, 1000);
+  }
 </script>
 
 <!-- label: cria o texto associado ao campo do input -->
 <!-- input: cria o campo em que o usuário vai digitar. valor será sincronizado com o variável name. -->
-<!-- if: se name tiver algum valor, <p> vai aparecer no navegador. -->
+<!-- if: se debouncedName existir, <p> vai aparecer no navegador. -->
 <main>
     
     <label for="nameInput">Digite um nome:</label> 
@@ -17,8 +33,8 @@
         placeholder="Ex: Alberto"
     />
 
-    {#if name}
-        <p>Você digitou: {name}</p>
+    {#if debouncedName}
+        <p>Você digitou: {debouncedName}</p>
     {/if}
 </main>
 
